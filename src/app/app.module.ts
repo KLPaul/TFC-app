872041bff483d1ba9b07a1a0d83c348cadea4fd9
KLPaul <klever.guaicha.est@tecazuay.edu.ec>
2023-07-08 10:59:08 -0500

    se agrega el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginRegisComponent } from './login-regis/login-regis/login-regis.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginRegisComponent
   ],
   imports: [
     BrowserModule,
